af6bd4bc15c9902a3fa9c6321db6976704e6f542
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2012-09-09 17:17:27 +0200

    add comments to tax calculator

--- lib/bookie/tax_calculator.rb
+++ lib/bookie/tax_calculator.rb
@@ -1,5 +1,9 @@
 require_relative 'calculations_helper'
 
+# This system follows the swedish tax system
+# Right now, it is a very naive implementation that claims that you have to pay 100% tax on the money you take out.
+# This is for the most part correct, even though there are a lot of variables to get a more accurate number
+
 class TaxCalculator
   include CalculationsHelper
   TAX_LEVEL = 2
