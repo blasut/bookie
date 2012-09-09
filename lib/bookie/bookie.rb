e5a3ebc276791d5ba4ee7e50b98a559db0166fcc
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2012-09-09 15:40:55 +0200

    add first draft of functionality

---
+++ lib/bookie/bookie.rb
@@ -0,0 +1,107 @@
+require 'date'
+
+class Bookie
+  attr_accessor :entries
+  include Comparable
+
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(entry)
+    @entries << entry  
+  end
+
+  def total_vat(from=nil, to=nil)
+    # Get all the entries within the dates
+    # Calculate the total vat from the income
+    # And subtract the total vat from the expenes
+    incomes = total_vat_per_entry_group(incomes(from, to))
+    expenses = total_vat_per_entry_group(expenses(from, to))
+    incomes - expenses
+  end
+
+  def money_left
+    incomes_total = total(incomes)
+    expenses_total = total(expenses)
+    (incomes_total - expenses_total - total_vat) - (total(salaries) * 2)
+  end
+
+  def tax_result
+    total(salaries) * 2
+  end
+
+  def total(entries)
+    entries.map(&:money).inject(0, &:+)
+  end
+
+  def total_vat_per_entry_group(entries)
+    entries.map(&:vat).inject(0, &:+)
+  end
+
+  def expenses(from=nil, to=nil)
+    @entries.select do |entry|
+      if from && to
+         entry.expense? && within_range(from, to, entry.date)
+      else
+        entry.expense?
+      end
+    end
+  end
+
+  def incomes(from=nil, to=nil)
+    @entries.select do |entry|
+      if from && to
+         entry.income? && within_range(from, to, entry.date)
+      else
+        entry.income?
+      end
+    end
+  end
+
+  def salaries
+    @entries.select do |entry|
+      entry.salary?
+    end
+  end
+
+  def within_range(from, to, date)
+    datetime = date.to_datetime
+    datetime >= from.to_datetime && datetime <= to.to_datetime
+  end
+
+end
+
+class Entry
+  attr_reader :date, :money
+
+  def initialize(money, date, type)
+    @money = money
+    @date = date
+    case type
+    when :income
+      @income = true
+    when :expense
+      @expense = true
+    when :salary
+      @salary = true
+    end
+  end
+
+  def vat
+    @money * 0.2
+  end
+
+  def income?
+    @income
+  end
+
+  def salary?
+    @salary
+  end
+  
+  def expense?
+    @expense
+  end
+end
+
--- spec/integration/bookie_integration_spec.rb
+++ spec/integration/bookie_integration_spec.rb
@@ -1,9 +1,62 @@
-describe Bookie do
-  it "saves a record of income"
-  it "saves a record of cost"
-  it "saves a record of salary"
+require_relative '../../lib/bookie/bookie'
 
-  it "calculates the VAT result for the specified monthly period"
-  it "calculates the tax result for a year"
-  it "calculates money left after VAT and taxes"
+describe Bookie do
+  let(:bookie) { Bookie.new }
+  let(:income) { Entry.new(5000, Date.new(2012, 01, 9), :income) }
+  let(:expense) { Entry.new(500, Date.new(2012, 01, 9), :expense) }
+  let(:salary) { Entry.new(100, Date.new(2012, 10, 9), :salary) }
+
+  before :each do
+    bookie.add_entry(income)
+    bookie.add_entry(expense)
+    bookie.add_entry(salary)
+  end
+
+  describe "saves a record" do
+    it "should handle multiple kinds of entries" do
+      bookie.salaries.should == [salary]
+      bookie.expenses.should == [expense]
+      bookie.incomes.should == [income]
+    end
+  end
+
+  describe "within range" do
+    it "should find stuff within a date range" do
+      from = Date.new(2012, 01, 9)
+      date = Date.new(2012, 02, 5)
+      to = Date.new(2012, 03, 9)
+      bookie.within_range(from, to, date).should == true
+    end
+  end
+
+  describe "calculates" do
+    let(:income2) { Entry.new(5000, Date.new(2012, 01, 9), :income) }
+    let(:expense2) { Entry.new(500, Date.new(2012, 01, 9), :expense) }
+
+    before :each do
+      bookie.add_entry(income2)
+      bookie.add_entry(expense2)
+    end
+
+    it "the VAT result for the specified monthly period" do
+      # 2000 from the income
+      # - 200 from the expense
+      bookie.total_vat(Date.new(2012, 01, 01), Date.new(2012, 03, 31)).should == 1800 
+    end
+
+    it "the tax result for a year" do
+      # salary * 2
+      # Salary is 100
+      bookie.tax_result.should == 200
+    end
+
+    it "money left after VAT and taxes" do
+      # Tax result is the (income + expenses - vat) - salary * 2
+      # Income: 10000
+      # Expenses: 1000
+      # Total after VAT: 7200
+      # Minus salary * 2 : 7000
+      bookie.money_left.should == 7000
+    end
+  end
 end
