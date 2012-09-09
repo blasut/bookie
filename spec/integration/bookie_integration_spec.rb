09c93bf75ac5c021cab7f5d8328c945c1146066a
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2012-09-09 13:58:51 +0200

    add pending integration specs for describing high level behaviour

---
+++ spec/integration/bookie_integration_spec.rb
@@ -0,0 +1,9 @@
+describe Bookie do
+  it "saves a record of income"
+  it "saves a record of cost"
+  it "saves a record of salary"
+
+  it "calculates the VAT result for the specified monthly period"
+  it "calculates the tax result for a year"
+  it "calculates money left after VAT and taxes"
+end
